f5e6348dfb5a4a80562a2f6f8ab338115d244650
Andy Smith <andy.01smith@gmail.com>
2015-07-18 12:54:11 +0100

    Initial setup

---
+++ linkdots.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Vars
+dotDir=~/dotfiles
+oldDir=~/dotfiles_old
+dFile="aliases bashrc clusterssh tmux.conf tmuxinator vim vimrc zprezto"
+
+#Backup
+
+if [ ! -d $oldDir ] ; then
+	echo "Creating backup directory in $oldDir"
+	mkdir -p $oldDir
+	echo "...done"
+fi
+
+echo "changing to dot dir"
+cd $dotDir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $oldDir"
+for file in $files; do
+    mv ~/.$file $oldDir/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dotDir/$file ~/.$file
+done
